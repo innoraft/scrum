47bbed370b2f7bde4f6b19bac4b17aca58377370
Jitujain <jitendra.jain@innoraft.com>
2016-08-31 16:49:04 +0530

    Fix js error

--- docroot/sites/all/themes/adminimal_theme/js/scrum.js
+++ docroot/sites/all/themes/adminimal_theme/js/scrum.js
@@ -3,10 +3,18 @@
     attach: function(context, settings) {
 
       $('select[name="field_ct_scrum[und]"]').change(function() {
-        console.log($(this).val());
         var tid = $(this).val();
         $('#block-views-views-scrum-last-scrum select').val(tid);
         $('#block-views-views-scrum-last-scrum .form-submit').click();
+      });
+
+      $(window).load(function(){
+        var option = $('.node-ct_scrum-form .field-name-field-ct-scrum select option').size();
+        if(typeof option != 'undefined') {
+          tid = $('.node-ct_scrum-form .field-name-field-ct-scrum select option').val();
+          $('#block-views-views-scrum-last-scrum select').val(tid);
+          $('#block-views-views-scrum-last-scrum .form-submit').click();
+        }
       });
     }
   };
