c68f151348f51a66e97b0a77c407a4f2cc7209bc
Jitujain <jitendra.jain@innoraft.com>
2016-09-01 11:45:46 +0530

    Change scrum subject and mail body

--- docroot/sites/all/modules/custom/scrum_general_apis/scrum-mail-template.tpl.php
+++ docroot/sites/all/modules/custom/scrum_general_apis/scrum-mail-template.tpl.php
@@ -5,7 +5,7 @@
 &nbsp;&nbsp;You are scrum master of <span style="color:orange"><?php print $data['project_name'] ?></span>.
 </p>
 <p>
-&nbsp;&nbsp; Sorry to say, but you have not created today's scrum for your project. Please create today's scrum for your project.
+&nbsp;&nbsp; Sorry to say, it seems that you have missed today's scrum for your project. Please create today's scrum for your project.
 </p>
 <p>
 Thank You<br>
