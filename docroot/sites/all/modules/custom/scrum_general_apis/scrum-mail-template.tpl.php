55b7b3690561f93a886c82e40d6edfff308353a0
Jitujain <jitendra.jain@innoraft.com>
2016-09-01 11:13:17 +0530

    Different mails for different user

---
+++ docroot/sites/all/modules/custom/scrum_general_apis/scrum-mail-template.tpl.php
@@ -0,0 +1,13 @@
+<p>
+  Hello <?php print $data['scrum_master'] ?>
+</p>
+<p>
+&nbsp;&nbsp;You are scrum master of <span style="color:orange"><?php print $data['project_name'] ?></span>.
+</p>
+<p>
+&nbsp;&nbsp; Sorry to say, but you have not created today's scrum for your project. Please create today's scrum for your project.
+</p>
+<p>
+Thank You<br>
+Innoraft solution
+</p>
