2c445170f2dfc2aeab0c398e3323d5d49f2b56c9
Jitujain <jitendra.jain@innoraft.com>
2016-09-08 14:56:14 +0530

    fix mail for bad scrums

--- docroot/sites/all/modules/custom/scrum_general_apis/scrum-mail-template-bad-status.tpl.php
+++ docroot/sites/all/modules/custom/scrum_general_apis/scrum-mail-template-bad-status.tpl.php
@@ -1,16 +1,30 @@
 <p>
-  Hello <?php print $data['scrum_master'] ?>
+  Hello Admin
+    Following scrums have bad/worse status in their last scrum.
 </p>
-<p>
-&nbsp;&nbsp;You  scrum master of <span style="color:orange"><?php print $data['project_name'] ?></span>.
-</p>
-<p>
-&nbsp;&nbsp; Sorry to say, your project status is <span style="color:orange"><?php print $data['project_status'] ?></span>.
-</p>
-<?php if (!empty($data['summary'])): ?>
-&nbsp;&nbsp;<h4>Your Project summary is</h4>
-<?php print $data['summary'] ?>
-<?php endif; ?>
+
+<?php foreach($data as $value): ?>
+
+  <p>
+    <h4>Project Name</h4> : <?php print $value['project_name'] ?>
+  </p>
+  <p>
+    <h4>Scrum Master</h4> : <?php print $value['scrum_master'] ?>
+  </p>
+  <p>
+    <h4>Project Status<h4/> : <?php print $value['project_status'] ?></span>.
+  </p>
+  <p>
+    <h4>Scrum Date<h4/> : <?php print $value['date'] ?></span>.
+  </p>
+  <?php if (!empty($value['summary'])): ?>
+  <h4>Project summary</h4> :
+  <?php print $value['summary'] ?>
+  <?php endif; ?>
+
+
+<?php endforeach; ?>
+
 <p>
 Thank You<br>
 Innoraft solution
